0539596cf9abcf8178063e588dd3de0d8d456d3c
Sam Chen <165413013+samJunxuanChen@users.noreply.github.com>
2024-05-16 20:42:48 -0700

    Update Table-Global Gemographic Factors.R

--- scripts/Table-Global Gemographic Factors.R
+++ scripts/Table-Global Gemographic Factors.R
@@ -1,11 +1,11 @@
 library(knitr)
-# Read the Data file into R
+# Data
 data <- read.csv("DATA/Global Population Trends(2016-2022).csv")
 
 # Filter the data from 2018 to 2021 because the 2017 population data is missing
 filtered_data <- data[data$Year %in% c(2018, 2019, 2020, 2021), ]
 
-# Select the required variables
+# Select the variables that used in the chart
 selected_data <- filtered_data[, c("Country", "Year", "Total.Population", "Urban.Population", "Birth.Rate", "Life.Expectancy")]
 
 # Print the table using kable
